c2f3f17f3768acf4a33287b505cb8359d921fe4c
robsonjerlley <robsonjerlley.work@gmail.com>
2026-04-17 10:14:44 -0300

    Primeiro commit

---
+++ src/main/java/io/sertaoBit/odontocore/crm/Application.java
@@ -0,0 +1,13 @@
+package io.sertaoBit.odontocore.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
