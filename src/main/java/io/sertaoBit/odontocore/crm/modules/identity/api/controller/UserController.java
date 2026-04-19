c4803fae316ab67f5ede3073ee85fa3465a6380f
robsonjerlley <robsonjerlley.work@gmail.com>
2026-04-19 17:38:25 -0300

    Definindo UserController

---
+++ src/main/java/io/sertaoBit/odontocore/crm/modules/identity/api/controller/UserController.java
@@ -0,0 +1,60 @@
+package io.sertaoBit.odontocore.crm.modules.identity.api.controller;
+
+import io.sertaoBit.odontocore.crm.modules.identity.api.dto.request.UserCreateRequestDTO;
+import io.sertaoBit.odontocore.crm.modules.identity.api.dto.response.UserResponseDTO;
+import io.sertaoBit.odontocore.crm.modules.identity.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping
+    public ResponseEntity<UserResponseDTO>create(@Valid @RequestBody UserCreateRequestDTO requestDTO) {
+        UserResponseDTO userResponseDTO = userService.create(requestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userResponseDTO);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserResponseDTO>> findAll() {
+        return ResponseEntity.ok(userService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponseDTO> findById(@PathVariable UUID id) {
+        return ResponseEntity.ok(userService.findById(id));
+    }
+
+    @GetMapping("/username/{username}")
+    public ResponseEntity<UserResponseDTO>findByUsername(@PathVariable String username) {
+        return ResponseEntity.ok(userService.findByUsername(username));
+    }
+
+    @PatchMapping("/{username}/password")
+    public ResponseEntity<UserResponseDTO>updatePassword(
+            @PathVariable String username,
+            @RequestBody String newPassword ){
+
+        return ResponseEntity.ok(userService.updatePassword(username, newPassword));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable UUID id) {
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+}
