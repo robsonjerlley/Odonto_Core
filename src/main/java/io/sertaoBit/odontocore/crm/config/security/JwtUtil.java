85390e2db5d9d9fefcc02783420b4dde6d430246
robsonjerlley <robsonjerlley.work@gmail.com>
2026-05-06 20:46:45 -0300

    Definindo JwtUtil.java

--- src/main/java/io/sertaoBit/odontocore/crm/config/security/JwtUtil.java
+++ src/main/java/io/sertaoBit/odontocore/crm/config/security/JwtUtil.java
@@ -1,17 +1,63 @@
 package io.sertaoBit.odontocore.crm.config.security;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
 
 @Component
 public class JwtUtil {
 
-    @Bean
-    public AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setUserDetailsPasswordService(u);
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.expiration}")
+    private long expiration;
+
+
+    private SecretKey getSecretKey() {
+        byte[] keyBytes = Decoders.BASE64URL.decode(this.secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 
+
+    public String generateToken(UserDetails userDetails) {
+        return Jwts.builder()
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getSecretKey())
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parser()
+                .verifyWith(getSecretKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+
+    private boolean isExpired(String token) {
+        Date expired = Jwts.parser()
+                .verifyWith(getSecretKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getExpiration();
+        return expired.before(new Date());
+    }
+
+    public boolean isValid(String token, UserDetails userDetails) {
+        String username = extractUsername(token);
+        return username.equals(userDetails.getUsername()) && !isExpired(token);
+    }
 }
